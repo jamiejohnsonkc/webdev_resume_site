91807c90e57d3b918f8587e1b045f43fa9ebe9ad
Jamie Johnson <jamie@jamie-johnson.com>
2019-10-07 12:58:36 -0500

    final dev style updates

--- mobileOverlay.php
+++ mobileOverlay.php
@@ -10,37 +10,22 @@ if (!defined('ABSPATH')) {
 ?>
 
 <!-- Button to close the overlay navigation -->
+<div class="mesh"></div>
 <div class="container">
-    <?php get_template_part('site_glyph--rev'); ?>
-    <a href="javascript:void(0)"
-       class="closebtn"
-       onclick="closeNav()">&times;</a>
+    <?php get_template_part('site_glyph-overlay'); ?>
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 </div>
 <!-- Overlay content -->
 <div class="overlay__content">
 
     <div class="overlay__menu">
-        <a href="#"><img src="/wp-content/uploads/2019/09/home.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">home</span></a>
-        <a href="#"><img src="/wp-content/uploads/2019/09/code.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">projects</span></a>
-        <a href="#"><img src="/wp-content/uploads/2019/09/keyboard.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">skills</span></a>
-        <a href="#"><img src="/wp-content/uploads/2019/09/briefcase.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">priors</span></a>
-        <a href="#"><img src="/wp-content/uploads/2019/09/music.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">interests</span></a>
-        <a href="#"><img src="/wp-content/uploads/2019/09/download.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">resume</span></a>
-        <a href="#"><img src="/wp-content/uploads/2019/09/at.svg"
-                 class="style-svg icon__overlay-nav"
-                 alt="home"><span class="nav-item__overlay">contact</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/home.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">home</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/code.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">projects</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/keyboard.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">skills</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/briefcase.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">priors</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/music.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">interests</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/download.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">resume</span></a>
+        <a href="#"><img src="/wp-content/uploads/2019/09/at.svg" class="style-svg icon__overlay-nav" alt="home"><span class="nav-item__overlay">contact</span></a>
     </div>
 </div>
 
@@ -50,18 +35,13 @@ if (!defined('ABSPATH')) {
         <div class="footer__subhead">web development & design</div>
     </div>
     <div class="container__footer-icons">
-        <img src="/wp-content/uploads/2019/09/icon__skills-28.svg"
-             class="style-svg icon__contact--footer"
-             alt="github" />
-        <img src="/wp-content/uploads/2019/09/icon__skills-98.svg"
-             class="style-svg icon__contact--footer"
-             alt="reddit" />
-        <img src="/wp-content/uploads/2019/09/icon__skills-24.svg"
-             class="style-svg icon__contact--footer"
-             alt="linkedin" />
-        <img src="/wp-content/uploads/2019/09/icon__skills-83.svg"
-             class="style-svg icon__contact--footer"
-             alt="facebook" />
+    <div class="container__sidebar-icons">
+        <img src="/wp-content/uploads/2019/09/icon__skills-28.svg" class="style-svg icon__contact--sidebar" alt="github" />
+        <img src="/wp-content/uploads/2019/09/icon__skills-98.svg" class="style-svg icon__contact--sidebar" alt="reddit" />
+        <img src="/wp-content/uploads/2019/09/icon__skills-24.svg" class="style-svg icon__contact--sidebar" alt="linkedin" />
+        <img src="/wp-content/uploads/2019/09/icon__skills-83.svg" class="style-svg icon__contact--sidebar" alt="facebook" />
+    </div>
     </div>
 
 </div>
+
--- sidebar.php
+++ sidebar.php
@@ -9,39 +9,34 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
+<!-- Overlay content -->
+<div class="sidebar__content-container">
 
+    <div class="sidebar__menu">
+        <?php get_template_part('site_glyph'); ?>
+        <!-- <a href="#"><img src="/wp-content/uploads/2019/09/home.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">home</span></a> -->
+        <a href="#projects"><img src="/wp-content/uploads/2019/09/code.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">projects</span></a>
+        <a href="#skills"><img src="/wp-content/uploads/2019/09/keyboard.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">skills</span></a>
+        <a href="#priors"><img src="/wp-content/uploads/2019/09/briefcase.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">priors</span></a>
+        <a href="#about"><img src="/wp-content/uploads/2019/09/music.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">interests</span></a>
+        <a href="mailto:jamie@jamie-johnson.me"><img src="/wp-content/uploads/2019/09/at.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">write</span></a>
+        <a href="tel:913-588-8042"><img src="/wp-content/uploads/2019/10/phone.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">call</span></a>
+        <a href="http://resume.test/wp-content/uploads/2019/10/Jamie-Johnson-Frontend-Development.pdf"><img src="/wp-content/uploads/2019/09/download.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">resume</span></a>
+    </div>
 
-  <!-- Overlay content --> 
-  <div class="sidebar__content-container">
-
-  <div class="sidebar__menu">
-  <?php get_template_part('site_glyph'); ?>   
-  <!-- <a href="#"><img src="/wp-content/uploads/2019/09/home.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">home</span></a> -->
-    <a href="#"><img src="/wp-content/uploads/2019/09/code.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">projects</span></a>
-    <a href="#"><img src="/wp-content/uploads/2019/09/keyboard.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">skills</span></a>
-    <a href="#"><img src="/wp-content/uploads/2019/09/briefcase.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">priors</span></a>
-    <a href="#"><img src="/wp-content/uploads/2019/09/music.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">interests</span></a>
-    <a href="#"><img src="/wp-content/uploads/2019/09/download.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">resume</span></a>
-    <a href="#"><img src="/wp-content/uploads/2019/09/at.svg" class="style-svg icon__sidebar-nav" alt="home"><span class="nav-item__sidebar">email</span></a>
-  </div>
-
-
-
-  <!-- <div class="sidebar__footer">
+    <!-- <div class="sidebar__footer">
   <div class="sidebar-footer__text"> -->
     <!-- <div class="sidebar-footer__head">Jamie Johnson</div>
     <div class="sidebar-footer__subhead">website design & production</div> -->
-<!-- </div> -->
-  <div class="container__sidebar-icons">
-  <img src="/wp-content/uploads/2019/09/icon__skills-28.svg" class="style-svg icon__contact--sidebar" alt="github" />
-<img src="/wp-content/uploads/2019/09/icon__skills-98.svg" class="style-svg icon__contact--sidebar" alt="reddit"/>
-<img src="/wp-content/uploads/2019/09/icon__skills-24.svg" class="style-svg icon__contact--sidebar" alt="linkedin"/>
-<img src="/wp-content/uploads/2019/09/icon__skills-83.svg" class="style-svg icon__contact--sidebar" alt="facebook"/>
+    <!-- </div> -->
+    <div class="container__sidebar-icons">
+        <img src="/wp-content/uploads/2019/09/icon__skills-28.svg" class="style-svg icon__contact--sidebar" alt="github" />
+        <img src="/wp-content/uploads/2019/09/icon__skills-98.svg" class="style-svg icon__contact--sidebar" alt="reddit" />
+        <img src="/wp-content/uploads/2019/09/icon__skills-24.svg" class="style-svg icon__contact--sidebar" alt="linkedin" />
+        <img src="/wp-content/uploads/2019/09/icon__skills-83.svg" class="style-svg icon__contact--sidebar" alt="facebook" />
+    </div>
+
+    <!-- </div> -->
 </div>
-
-<!-- </div> -->
-</div>
-
-
 
 <!-- <span onclick="openNav()">open</span> -->
